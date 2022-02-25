e9e32d4c679d6c972efe2e194a35d7c183eb62bc
D Pham <d.pham@shopify.com>
2022-02-25 10:29:02 -0500

    Change delivery demo to percentage discount

--- discounts/typescript/delivery-discount-types/default/src/index.test.ts
+++ discounts/typescript/delivery-discount-types/default/src/index.test.ts
@@ -4,14 +4,27 @@ import * as rawPayload from './input.json';
 
 const payload = camelcaseKeys(rawPayload, {deep: true});
 
-describe('free shipping discount type', () => {
-  it('returns 100% discount targeting a delivery line', () => {
+describe('percentage shipping discount type', () => {
+  it('returns free shipping discount by default', () => {
     const result = main(payload);
 
     expect(result.discounts).toStrictEqual([
       expect.objectContaining({
         title: 'Free shipping',
         value: {type: 'percentage', value: 100},
+        allocations: [{lineIndex: 0}],
+      }),
+    ]);
+  });
+
+  it('returns configured percentage value if present', () => {
+    const configuration = {value: 20};
+    const result = main({...payload, configuration});
+
+    expect(result.discounts).toStrictEqual([
+      expect.objectContaining({
+        title: '20% off',
+        value: {type: 'percentage', value: 20},
         allocations: [{lineIndex: 0}],
       }),
     ]);
--- discounts/typescript/delivery-discount-types/default/src/index.ts
+++ discounts/typescript/delivery-discount-types/default/src/index.ts
@@ -5,11 +5,13 @@ type Output = DeliveryDiscountTypesAPI.Output;
 
 export const main = ({input, configuration}: Payload): Output => {
   console.log('[Delivery Demo]', configuration);
+  const percentage = configuration.value ? parseFloat(configuration.value) : 100;
+  const title = configuration.title ?? (percentage === 100 ? 'Free shipping' : `${percentage}% off`);
   return {
     discounts: input.deliveryLines.map((line) =>
       DeliveryDiscountTypesAPI.discount({
-        title: configuration.title || 'Free shipping',
-        value: Discounts.percentage(100),
+        title,
+        value: Discounts.percentage(percentage),
         lineIndex: line.index,
       }),
     ),
