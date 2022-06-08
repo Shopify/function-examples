fba84988cf8ea0468260887b1127793d799f58e0
andrewhassan <andrewihassan@gmail.com>
2022-06-08 16:54:01 -0400

    Format useAuthenticatedFetch

--- sample-apps/discount-functions-sample-app/web/frontend/hooks/useAuthenticatedFetch.js
+++ sample-apps/discount-functions-sample-app/web/frontend/hooks/useAuthenticatedFetch.js
@@ -1,6 +1,6 @@
-import { authenticatedFetch } from '@shopify/app-bridge-utils'
-import { useAppBridge } from '@shopify/app-bridge-react'
-import { Redirect } from '@shopify/app-bridge/actions'
+import { authenticatedFetch } from '@shopify/app-bridge-utils';
+import { useAppBridge } from '@shopify/app-bridge-react';
+import { Redirect } from '@shopify/app-bridge/actions';
 
 /**
  * A hook that returns an auth-aware fetch function.
@@ -15,28 +15,28 @@ import { Redirect } from '@shopify/app-bridge/actions'
  * @returns {Function} fetch function
  */
 export function useAuthenticatedFetch() {
-  const app = useAppBridge()
-  const fetchFunction = authenticatedFetch(app)
+  const app = useAppBridge();
+  const fetchFunction = authenticatedFetch(app);
 
   return async (uri, options) => {
-    const response = await fetchFunction(uri, options)
-    checkHeadersForReauthorization(response.headers, app)
-    return response
-  }
+    const response = await fetchFunction(uri, options);
+    checkHeadersForReauthorization(response.headers, app);
+    return response;
+  };
 }
 
 function checkHeadersForReauthorization(headers, app) {
   if (headers.get('X-Shopify-API-Request-Failure-Reauthorize') === '1') {
     const authUrlHeader =
       headers.get('X-Shopify-API-Request-Failure-Reauthorize-Url') ||
-      `/api/auth`
+      `/api/auth`;
 
-    const redirect = Redirect.create(app)
+    const redirect = Redirect.create(app);
     redirect.dispatch(
       Redirect.Action.REMOTE,
       authUrlHeader.startsWith('/')
         ? `https://${window.location.host}${authUrlHeader}`
-        : authUrlHeader
-    )
+        : authUrlHeader,
+    );
   }
 }
