e9a48a4c16fc9c9d029c7ae14c3bd834dd466676
Paulo Margarido <64600052+paulomarg@users.noreply.github.com>
2022-07-13 10:32:56 -0400

    Use offline access tokens

--- sample-apps/discounts-tutorial/web/frontend/components/providers/AppBridgeProvider.jsx
+++ sample-apps/discounts-tutorial/web/frontend/components/providers/AppBridgeProvider.jsx
@@ -48,17 +48,35 @@ export function AppBridgeProvider({ children }) {
     };
   });
 
-  if (!process.env.SHOPIFY_API_KEY) {
+  if (!process.env.SHOPIFY_API_KEY || !appBridgeConfig.host) {
+    const bannerProps = !process.env.SHOPIFY_API_KEY
+      ? {
+          title: "Missing Shopify API Key",
+          children: (
+            <>
+              Your app is running without the SHOPIFY_API_KEY environment
+              variable. Please ensure that it is set when running or building
+              your React app.
+            </>
+          ),
+        }
+      : {
+          title: "Missing host query argument",
+          children: (
+            <>
+              Your app can only load if the URL has a <b>host</b> argument.
+              Please ensure that it is set, or access your app using the
+              Partners Dashboard <b>Test your app</b> feature
+            </>
+          ),
+        };
+
     return (
       <Page narrowWidth>
         <Layout>
           <Layout.Section>
             <div style={{ marginTop: "100px" }}>
-              <Banner title="Missing Shopify API key" status="critical">
-                Your app is running without the SHOPIFY_API_KEY environment
-                variable. Please ensure that it is set when running or building
-                your React app.
-              </Banner>
+              <Banner {...bannerProps} status="critical" />
             </div>
           </Layout.Section>
         </Layout>
