43125d3d9fe581a834c4de1daa72cc69962756a0
Marlon Marcello <marlon.marcello@shopify.com>
2022-07-29 06:02:54 -0700

    fix: updates util description

--- util/expand-liquid.js
+++ util/expand-liquid.js
@@ -1,6 +1,6 @@
 /***
  * For local sample development / use without the Shopify CLI only.
- * Shopify CLI will expand liquid template files when these templates are used.
+ * This script replaces the liquid expansion functionality of the Shopify CLI for use cases where the Shopify CLI is not being used, like local sample development.
  */
 
  import glob from 'fast-glob';
@@ -9,45 +9,42 @@
  import fs from 'node:fs/promises';
  import { existsSync } from 'fs';
  import toml from '@iarna/toml';
- 
+
  const samplePath = path.join(process.cwd(), 'sample-apps');
  const samples = (await fs.readdir(samplePath, { withFileTypes: true }))
      .filter(dirent => dirent.isDirectory())
      .map(dirent => path.join(samplePath, dirent.name));
- 
+
  for (const sample of samples) {
- 
-     const configPath = path.join(sample, 'shopify.app.toml');
-     if (!(existsSync(configPath))) {
-         console.error(`${configPath} does not exist`);
-         continue;
-     }
-     const appConfig = toml.parse(await fs.readFile(configPath, 'utf8'));
- 
-     console.log(`Expanding liquid templates for ${appConfig.name}`);
- 
-     const liquidData = {
-         app_name: appConfig.name,
-         dependency_manager: 'yarn'
-     }
- 
-     const entries = await glob(
-         [path.join(sample, '**/*.liquid')],
-         {
-             dot: true,
-             ignore: ['**/node_modules'],
-         });
-     
-     for (const entry of entries) {
-         const engine = new Liquid();
-         const content = await fs.readFile(entry, 'utf8');
-         const rendered = await engine.renderFile(entry, liquidData);
-         const outputPath = entry.replace('.liquid', '');
-         await fs.writeFile(outputPath, rendered);
-         console.log(outputPath);
-     }
- 
-     console.log();
+   const configPath = path.join(sample, "shopify.app.toml");
+   if (!existsSync(configPath)) {
+     console.error(`${configPath} does not exist`);
+     continue;
+   }
+   const appConfig = toml.parse(await fs.readFile(configPath, "utf8"));
+
+   console.log(`Expanding liquid templates for ${appConfig.name}`);
+
+   const liquidData = {
+     app_name: appConfig.name,
+     dependency_manager: "yarn",
+   };
+
+   const entries = await glob([path.join(sample, "**/*.liquid")], {
+     dot: true,
+     ignore: ["**/node_modules"],
+   });
+
+   for (const entry of entries) {
+     const engine = new Liquid();
+     const content = await fs.readFile(entry, "utf8");
+     const rendered = await engine.renderFile(entry, liquidData);
+     const outputPath = entry.replace(".liquid", "");
+     await fs.writeFile(outputPath, rendered);
+     console.log(outputPath);
+   }
+
+   console.log();
  }
- 
+
  console.log('The above files should be added to .gitignore if they have not already been added.');
