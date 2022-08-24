aafed4229e74d6b5fdb322defcf92658d66c8173
Thomas Devisscher <thomas.devisscher@shopify.com>
2022-08-24 09:19:23 -0400

    fix: update vite config in payment-customization-hide

--- sample-apps/payment-customization-hide/web/frontend/vite.config.js
+++ sample-apps/payment-customization-hide/web/frontend/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import https from "https";
-
+import react from '@vitejs/plugin-react';
 if (
   process.env.npm_lifecycle_event === "build" &&
   !process.env.CI &&
@@ -22,44 +22,39 @@ const proxyOptions = {
 
 const host = process.env.HOST
   ? process.env.HOST.replace(/https:\/\//, "")
-  : undefined;
+  : "localhost";
 
 // HMR doesn't work on Firefox using localhost, so you can temporarily get that to work by setting the
 // SHOPIFY_VITE_HMR_USE_POLLING env var when running this
 let hmrConfig;
-if (process.env.SHOPIFY_VITE_HMR_USE_POLLING) {
-  hmrConfig = {
-    server: https.createServer(),
-  };
-} else if (process.env.SHOPIFY_VITE_HMR_USE_WSS) {
-  hmrConfig = {
-    protocol: host ? "wss" : "ws",
-    host: host || "localhost",
-    port: process.env.FRONTEND_PORT,
-    clientPort: 443,
-  };
-} else {
+if (host === "localhost") {
   hmrConfig = {
     protocol: "ws",
     host: "localhost",
     port: 64999,
     clientPort: 64999,
   };
+} else {
+  hmrConfig = {
+    protocol: "wss",
+    host: host,
+    port: process.env.FRONTEND_PORT,
+    clientPort: 443,
+  };
 }
 
 export default defineConfig({
   root: dirname(fileURLToPath(import.meta.url)),
+  plugins: [react()],
   define: {
     "process.env.SHOPIFY_API_KEY": JSON.stringify(process.env.SHOPIFY_API_KEY),
   },
-  esbuild: {
-    jsxInject: `import React from 'react'`,
-  },
+
   resolve: {
     preserveSymlinks: true,
   },
   server: {
-    host: process.env.SHOPIFY_VITE_HMR_USE_WSS ? "0.0.0.0" : "localhost",
+    host: "localhost",
     port: process.env.FRONTEND_PORT,
     hmr: hmrConfig,
     proxy: {
