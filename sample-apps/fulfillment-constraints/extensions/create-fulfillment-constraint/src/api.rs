b3bd7ed2eab7cb97618352813a570228235b7f9d
Joshua Arts <joshua.arts@shopify.com>
2022-10-19 21:33:55 -0400

    Support locations and MustShipFrom

--- sample-apps/fulfillment-constraints/extensions/create-fulfillment-constraint/src/api.rs
+++ sample-apps/fulfillment-constraints/extensions/create-fulfillment-constraint/src/api.rs
@@ -6,12 +6,30 @@ pub type ID = String;
 #[serde(rename_all = "camelCase")]
 pub struct Input {
     pub cart: Cart,
+    pub locations: Vec<Location>,
 }
 
+// #[derive(Clone, Debug, Serialize)]
+// pub struct FulfillmentConstraint {
+//     pub constrained_by: String,
+//     pub line_items: Vec<ID>,
+// }
+
+// #[serde_as]
 #[derive(Clone, Debug, Serialize)]
-pub struct FulfillmentConstraint {
-    pub constrained_by: String,
-    pub line_items: Vec<ID>,
+#[serde(rename_all(serialize = "camelCase"))]
+pub enum FulfillmentConstraint {
+    #[serde(rename_all(serialize = "camelCase"))]
+    MustShipTogether {
+        r#type: String,
+        line_items: Vec<ID>,
+    },
+    #[serde(rename_all(serialize = "camelCase"))]
+    MustShipFrom {
+        r#type: String,
+        line_items: Vec<ID>,
+        location_id: String,
+    },
 }
 
 #[derive(Clone, Debug, Serialize)]
@@ -33,10 +51,18 @@ pub struct CartLine {
 #[derive(Clone, Debug, Deserialize, PartialEq)]
 pub struct Merchandise {
     pub id: Option<ID>,
+    pub sku: String,
     pub metafield: Option<Metafield>,
 }
 
 #[derive(Clone, Debug, Deserialize, PartialEq)]
 pub struct Metafield {
     pub value: String,
+}
+
+#[derive(Clone, Debug, Deserialize, PartialEq)]
+pub struct Location {
+    pub id: ID,
+    pub name: String,
+    pub address1: String,
 }
--- sample-apps/fulfillment-constraints/extensions/create-fulfillment-constraint/src/main.rs
+++ sample-apps/fulfillment-constraints/extensions/create-fulfillment-constraint/src/main.rs
@@ -7,6 +7,8 @@ use std::collections::hash_map::{Entry, HashMap};
 static MUST_BE_FULFILLED_TOGETHER: &str = "MUST_BE_FULFILLED_TOGETHER";
 static MUST_NOT_BE_FULFILLED_TOGETHER: &str = "MUST_NOT_BE_FULFILLED_TOGETHER";
 
+static MUST_SHIP_FROM: &str = "MUST_SHIP_FROM";
+
 static NOT_FOUND_RESULT: &str = "NOT_FOUND";
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -20,7 +22,56 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 fn function(input: Input) -> Result<FunctionResult, Box<dyn std::error::Error>> {
     let line_item_groups = build_line_item_groups(&input.cart.lines);
 
-    Ok(build_result(line_item_groups))
+    let fulfillment_constraints = build_constraints(line_item_groups, &input.cart.lines, &input.locations);
+
+    Ok(
+        FunctionResult {
+            fulfillment_constraints: fulfillment_constraints,
+        }
+    )
+}
+
+fn build_constraints(groups: HashMap<String, Vec<String>>, cart_lines: &[CartLine], locations: &[Location]) -> Vec<FulfillmentConstraint> {
+    let mut fulfillment_constraints: Vec<FulfillmentConstraint> = Vec::new();
+
+    // Build MustShipTogether fulfillment constraints based off of bundle identifier.
+    for (bundle_identifier, line_item_ids) in groups {
+        if bundle_identifier == NOT_FOUND_RESULT.to_string() || line_item_ids.len() < 2 { continue; }
+        let fulfillment_constraint = FulfillmentConstraint::MustShipTogether {
+            r#type: MUST_BE_FULFILLED_TOGETHER.to_string(),
+            line_items: (&line_item_ids).to_vec(),
+        };
+
+        fulfillment_constraints.push(fulfillment_constraint);
+    }
+
+    // Build MustShipFrom fulfillment constraints based sku with hardcoded location.
+    let mut location_id = NOT_FOUND_RESULT;
+    for location in locations {
+        if location.name == "767 Labadie View".to_string() {
+            location_id = &location.id;
+        }
+    }
+
+    if location_id == NOT_FOUND_RESULT {
+        return Vec::new();
+    }
+
+    for cart_line in cart_lines {
+        let sku = &cart_line.merchandise.sku;
+
+        if sku.to_string() == "SKU_ONE".to_string() {
+            let fulfillment_constraint = FulfillmentConstraint::MustShipFrom {
+                r#type: MUST_SHIP_FROM.to_string(),
+                line_items: vec![cart_line.id.clone()],
+                location_id: location_id.to_string(),
+            };
+    
+            fulfillment_constraints.push(fulfillment_constraint);
+        }
+    }
+
+    return fulfillment_constraints;
 }
 
 fn build_line_item_groups(cart_lines: &[CartLine]) -> HashMap<String, Vec<String>> {
@@ -40,22 +91,4 @@ fn build_line_item_groups(cart_lines: &[CartLine]) -> HashMap<String, Vec<String
     }
 
     return groups;
-}
-
-fn build_result(groups: HashMap<String, Vec<String>>) -> FunctionResult {
-    let mut fulfillment_constraints: Vec<FulfillmentConstraint> = Vec::new();
-
-    for (bundle_identifier, line_item_ids) in groups {
-        if bundle_identifier == NOT_FOUND_RESULT.to_string() || line_item_ids.len() < 2 { continue; }
-        let fulfillment_constraint = FulfillmentConstraint {
-            constrained_by: MUST_BE_FULFILLED_TOGETHER.to_string(),
-            line_items: (&line_item_ids).to_vec(),
-        };
-    
-        fulfillment_constraints.push(fulfillment_constraint);
-    }
-
-    FunctionResult {
-        fulfillment_constraints: fulfillment_constraints
-    }
 }
