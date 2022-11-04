c324e851963cd62b43ee35d401ffc66b380cace4
Delta Pham <d.pham@shopify.com>
2022-11-04 10:29:56 -0400

    Delete payment-customization-hide sample app

--- sample-apps/payment-customization-hide/web/frontend/components/providers/index.js
+++
@@ -1,3 +0,0 @@
-export { AppBridgeProvider } from "./AppBridgeProvider";
-export { GraphQLProvider } from "./GraphQLProvider";
-export { PolarisProvider } from "./PolarisProvider";
--- sample-apps/payment-customization-hide/web/frontend/hooks/index.js
+++
@@ -1,4 +0,0 @@
-export { useAppQuery } from "./useAppQuery";
-export { useAuthenticatedFetch } from "./useAuthenticatedFetch";
-export { useShopifyMutation } from "./useShopifyMutation";
-export { useShopifyQuery } from "./useShopifyQuery";
