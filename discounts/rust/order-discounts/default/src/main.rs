94456e8fdeff751b2835123b1ee6791faff13680
Nick Wesselman <27013789+nickwesselman@users.noreply.github.com>
2023-01-12 16:13:39 -0500

    updated order discount template for rust crate
    * moved tests into separate file for consistency

--- discounts/rust/order-discounts/default/src/main.rs
+++ discounts/rust/order-discounts/default/src/main.rs
@@ -1,11 +1,19 @@
+use shopify_function::prelude::*;
+use shopify_function::Result;
+
 use serde::{Deserialize, Serialize};
 
-mod api;
-use api::*;
+generate_types!(
+    query_path = "./input.graphql",
+    schema_path = "./schema.graphql"
+);
 
-#[derive(Clone, Debug, Serialize, Deserialize, Default)]
-#[serde(rename_all = "camelCase")]
-pub struct Configuration {}
+
+#[derive(Serialize, Deserialize, Default, PartialEq)]
+#[serde(rename_all(deserialize = "camelCase"))]
+struct Configuration {
+
+}
 
 impl Configuration {
     fn from_str(value: &str) -> Self {
@@ -13,78 +21,24 @@ impl Configuration {
     }
 }
 
-impl input::Input {
-    pub fn configuration(&self) -> Configuration {
-        match &self.discount_node.metafield {
-            Some(input::Metafield { value }) => Configuration::from_str(value),
-            None => Configuration::default(),
-        }
-    }
-}
-
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let input: input::Input = serde_json::from_reader(std::io::BufReader::new(std::io::stdin()))?;
-    let mut out = std::io::stdout();
-    let mut serializer = serde_json::Serializer::new(&mut out);
-    function(input)?.serialize(&mut serializer)?;
-    Ok(())
-}
-
-fn function(input: input::Input) -> Result<FunctionResult, Box<dyn std::error::Error>> {
-    let _config = input.configuration();
-    Ok(FunctionResult {
+#[shopify_function]
+fn function(input: input::ResponseData) -> Result<output::FunctionResult> {
+    let no_discount = output::FunctionResult {
         discounts: vec![],
-        discount_application_strategy: DiscountApplicationStrategy::First,
+        discount_application_strategy: output::DiscountApplicationStrategy::FIRST,
+    };
+
+    let _config = match input.discount_node.metafield {
+        Some(input::InputDiscountNodeMetafield { value }) => 
+            Configuration::from_str(&value),
+        None => return Ok(no_discount),
+    };
+
+    Ok(output::FunctionResult {
+        discounts: vec![],
+        discount_application_strategy: output::DiscountApplicationStrategy::FIRST,
     })
 }
 
 #[cfg(test)]
-mod tests {
-    use super::*;
-
-    fn input(config: Option<Configuration>) -> input::Input {
-        input::Input {
-            discount_node: input::DiscountNode {
-                metafield: Some(input::Metafield {
-                    value: serde_json::to_string(&config.unwrap_or_default()).unwrap()
-                }),
-            },
-        }
-    }
-
-    #[test]
-    fn test_discount_with_no_configuration() {
-        let input = input(None);
-        let handle_result = serde_json::json!(function(input).unwrap());
-
-        let expected_handle_result = serde_json::json!({
-            "discounts": [],
-            "discountApplicationStrategy": "FIRST",
-        });
-        assert_eq!(handle_result, expected_handle_result);
-    }
-
-    #[test]
-    fn test_discount_with_configuration() {
-        let input = input(Some(Configuration {}));
-        let handle_result = serde_json::json!(function(input).unwrap());
-
-        let expected_handle_result = serde_json::json!({
-            "discounts": [],
-            "discountApplicationStrategy": "FIRST",
-        });
-        assert_eq!(handle_result, expected_handle_result);
-    }
-
-    #[test]
-    fn test_input_deserialization() {
-        let input_json = r#"
-        {
-            "discountNode": { "metafield": { "value": "{}" } }
-        }
-        "#;
-
-        let expected_input = input(Some(Configuration {}));
-        assert_eq!(expected_input, serde_json::from_str::<input::Input>(input_json).unwrap());
-    }
-}
+mod tests;
