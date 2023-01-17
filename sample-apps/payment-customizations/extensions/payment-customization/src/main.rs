9c64317add0e2109c5f43ecd6d1325c3d12eed24
Andrew Hassan <andrew.hassan@shopify.com>
2023-01-17 16:35:33 -0500

    Payment customization tutorial - add configuration

--- sample-apps/payment-customizations/extensions/payment-customization/src/main.rs
+++ sample-apps/payment-customizations/extensions/payment-customization/src/main.rs
@@ -1,38 +1,60 @@
 use shopify_function::prelude::*;
 use shopify_function::Result;
 
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 
-// Use the shopify_function crate to generate structs for the function input and output
 generate_types!(
     query_path = "./input.graphql",
     schema_path = "./schema.graphql"
 );
 
-// Use the shopify_function crate to declare your function entrypoint
+// Create a structure that matches the JSON structure that you'll use for your configuration
+#[derive(Serialize, Deserialize, Default, PartialEq)]
+#[serde(rename_all(deserialize = "camelCase"))]
+struct Configuration {
+    payment_method_name: String,
+    cart_total: f64,
+}
+
+// Parse the JSON metafield value using serde
+impl Configuration {
+    fn from_str(value: &str) -> Self {
+        serde_json::from_str(value).expect("Unable to parse configuration value from metafield")
+    }
+}
+
 #[shopify_function]
 fn function(input: input::ResponseData) -> Result<output::FunctionResult> {
     let no_changes = output::FunctionResult { operations: vec![] };
 
-    // Get the cart total from the function input, and return early if it's below 100
+    // Get the configuration from the metafield on your function owner
+    let config = match input.payment_customization.metafield {
+        Some(input::InputPaymentCustomizationMetafield { value }) => {
+            Configuration::from_str(&value)
+        }
+        None => return Ok(no_changes),
+    };
+
+    // Use the configured cart total instead of a hardcoded value
     let cart_total = input.cart.cost.total_amount.amount.parse::<f64>().unwrap();
-    if cart_total < 100.0 {
-        // You can use STDERR for debug logs in your function
+    if cart_total < config.cart_total {
         eprintln!("Cart total is not high enough, no need to hide the payment method.");
         return Ok(no_changes);
     }
 
-    // Find the payment method to hide, and create a hide output operation from it
-    // (this will be None if not found)
+    // Use the configured payment method name instead of a hardcoded value
     let hide_payment_method = input
         .payment_methods
         .iter()
-        .find(|&method| method.name.contains(&"Cash on Delivery".to_string()))
+        .find(|&method| {
+            method
+                .name
+                .contains(&config.payment_method_name.to_string())
+        })
         .map(|method| output::HideOperation {
             payment_method_id: method.id.to_string(),
         });
 
-    // The shopify_function crate serializes your function result and write it to STDOUT
     Ok(output::FunctionResult {
         operations: vec![output::Operation {
             hide: hide_payment_method,
