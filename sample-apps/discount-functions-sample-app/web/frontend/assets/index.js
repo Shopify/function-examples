c3a909b1b137f873792258e75e99e9089b858a5a
Andrew Hassan <andrewihassan@gmail.com>
2023-01-20 17:06:49 -0500

    Remove discounts sample app

--- sample-apps/discount-functions-sample-app/web/frontend/assets/index.js
+++
@@ -1,1 +0,0 @@
-export { default as notFoundImage } from "./empty-state.svg";
--- sample-apps/discount-functions-sample-app/web/frontend/components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as DiscountCreatePage } from "./DiscountCreatePage";
-export { default as DiscountDetailsPage } from "./DiscountDetailsPage";
-export { default as VariantList } from "./VariantList";
-export * from "./providers";
-export * from "./function-configuration";
