7247ec0c31fe20dcd3dceea7464a233281340bbc
Lars Hoffbeck <lars.hoffbeck@shopify.com>
2023-07-05 13:48:10 -0400

    Updated to match JS version

--- sample-apps/bundles-cart-transform/extensions/cart-expand-ts/src/index.ts
+++ sample-apps/bundles-cart-transform/extensions/cart-expand-ts/src/index.ts
@@ -3,6 +3,7 @@ import type {
   FunctionResult,
   Scalars,
   CartOperation,
+  ExpandOperation,
 } from "../generated/api";
 
 type RequireStrict<T> = Required<{
@@ -14,33 +15,17 @@ type ProductVariant = RequireStrict<
   Extract<Merchandise, { __typename: "ProductVariant" }>
 >;
 
+const NO_CHANGES: FunctionResult = {
+  operations: [],
+};
+
 export default (input: InputQuery): FunctionResult => {
   const operations = input.cart.lines.reduce<Array<CartOperation>>(
-    (acc, {id: cartLineId, merchandise}) => {
-      if (canExpand(merchandise)) {
-        const componentReferences: Scalars["ID"][] = JSON.parse(
-          merchandise.componentReferences.value
-        );
-        const componentQuantities: Scalars["Int"][] = JSON.parse(
-          merchandise.componentQuantities.value
-        );
+    (acc, cartLine) => {
+      const expandOperation = optionallyBuildExpandOperation(cartLine);
 
-        if (!validateMetafields(componentReferences, componentQuantities)) {
-          throw new Error("Invalid bundle composition");
-        }
-
-        return [
-          ...acc,
-          {
-            expand: {
-              cartLineId,
-              expandedCartItems: componentReferences.map((merchandiseId, index) => ({
-                merchandiseId,
-                quantity: componentQuantities[index],
-              })),
-            },
-          },
-        ];
+      if (expandOperation) {
+        return [...acc, { expand: expandOperation }];
       }
 
       return acc;
@@ -48,10 +33,33 @@ export default (input: InputQuery): FunctionResult => {
     []
   );
 
-  return {
-    operations,
-  };
+  return operations.length > 0 ? { operations } : NO_CHANGES;
 };
+
+function optionallyBuildExpandOperation({ id: cartLineId, merchandise }): ExpandOperation | null {
+  if (canExpand(merchandise)) {
+    const componentReferences: Scalars["ID"][] = JSON.parse(
+      merchandise.componentReferences.value
+    );
+    const componentQuantities: Scalars["Int"][] = JSON.parse(
+      merchandise.componentQuantities.value
+    );
+
+    if (!validateMetafields(componentReferences, componentQuantities)) {
+      throw new Error("Invalid bundle composition");
+    }
+
+    return {
+      cartLineId,
+      expandedCartItems: componentReferences.map((merchandiseId, index) => ({
+        merchandiseId,
+        quantity: componentQuantities[index],
+      })),
+    };
+  }
+
+  return null;
+}
 
 function canExpand(merchandise: Merchandise): merchandise is ProductVariant {
   return (
@@ -61,6 +69,15 @@ function canExpand(merchandise: Merchandise): merchandise is ProductVariant {
   );
 }
 
-function validateMetafields(componentReferences: Scalars["ID"][], componentQuantities: Scalars["Int"][], ) {
-  return componentReferences.length !== componentQuantities.length && componentReferences.length > 0;
+/**
+ * Returns true if component references have matching quantities and there is at least one component.
+ */
+function validateMetafields(
+  componentReferences: Scalars["ID"][],
+  componentQuantities: Scalars["Int"][]
+) {
+  return (
+    componentReferences.length === componentQuantities.length &&
+    componentReferences.length > 0
+  );
 }
