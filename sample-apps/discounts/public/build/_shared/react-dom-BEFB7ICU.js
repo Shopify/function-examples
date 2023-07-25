d9dbafa3d8a051498162d987b11fc35785defc37
Andrew Hassan <andrewihassan@gmail.com>
2023-07-25 14:34:40 -0400

    Remove public/build artifacts

--- sample-apps/discounts/public/build/_shared/react-dom-BEFB7ICU.js
+++
@@ -1,7 +0,0 @@
-import {
-  require_react_dom
-} from "/build/_shared/chunk-GIAAE3CH.js";
-import "/build/_shared/chunk-BOXFZXVX.js";
-import "/build/_shared/chunk-PNG5AS42.js";
-export default require_react_dom();
-//# sourceMappingURL=/build/_shared/react-dom-BEFB7ICU.js.map
