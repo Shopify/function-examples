263c86a3d5d530d07489b70a5db3094fe69ce367
Renxia Wang <renxia.wang@shopify.com>
2023-09-07 16:27:07 -0400

    Add examples for checkout completion target customization to support CLI

---
+++ checkout/rust/completion-target-customization/default/src/main.rs
@@ -0,0 +1,36 @@
+use shopify_function::prelude::*;
+use shopify_function::Result;
+
+use serde::{Deserialize, Serialize};
+
+generate_types!(
+    query_path = "./input.graphql",
+    schema_path = "./schema.graphql"
+);
+
+#[derive(Serialize, Deserialize, Default, PartialEq)]
+#[serde(rename_all(deserialize = "camelCase"))]
+struct Configuration {}
+
+impl Configuration {
+    fn from_str(value: &str) -> Self {
+        serde_json::from_str(value).expect("Unable to parse configuration value from metafield")
+    }
+}
+
+#[shopify_function]
+fn function(input: input::ResponseData) -> Result<output::FunctionResult> {
+    let no_changes = output::FunctionResult { operations: vec![] };
+
+    let _config = match input.checkout_completion_target_customization.metafield {
+        Some(input::InputCheckoutCompletionTargetCustomizationMetafield { value }) => {
+            Configuration::from_str(&value)
+        }
+        None => return Ok(no_changes),
+    };
+
+    Ok(output::FunctionResult { operations: vec![] })
+}
+
+#[cfg(test)]
+mod tests;
