e60f056c3d8c8cecd3c93b375ca388877a6f92f2
Nick Wesselman <27013789+nickwesselman@users.noreply.github.com>
2023-09-25 14:03:48 -0400

    updated Rust crate for product discounts

---
+++ discounts/rust/product-discounts/default/src/run.rs
@@ -0,0 +1,41 @@
+use shopify_function::prelude::*;
+use shopify_function::Result;
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Default, PartialEq)]
+#[serde(rename_all(deserialize = "camelCase"))]
+struct Configuration {}
+
+impl Configuration {
+    fn from_str(value: &str) -> Self {
+        serde_json::from_str(value).expect("Unable to parse configuration value from metafield")
+    }
+}
+
+#[shopify_function_target(query_path = "src/run.graphql", schema_path = "schema.graphql")]
+fn run(input: input::ResponseData) -> Result<output::FunctionResult> {
+    let no_discount = output::FunctionResult {
+        discounts: vec![],
+        discount_application_strategy: output::DiscountApplicationStrategy::FIRST,
+    };
+
+    let _config = match input.discount_node.metafield {
+        Some(input::InputDiscountNodeMetafield { value }) => Configuration::from_str(&value),
+        None => return Ok(no_discount),
+    };
+
+    Ok(output::FunctionResult {
+        discounts: vec![],
+        discount_application_strategy: output::DiscountApplicationStrategy::FIRST,
+    })
+}
+
+/*
+ * unresolved module, can't find module file: run/tests.rs, or run/tests/mod.rs
+ * TODO: Should we move the tests inline, since that's more standard for Rust?
+
+#[cfg(test)]
+mod tests;
+
+*/
