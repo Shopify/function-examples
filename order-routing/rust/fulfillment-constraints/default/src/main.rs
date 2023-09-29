8307b18426c212b69221b13d9e1460c33c7f9d55
Nick Wesselman <27013789+nickwesselman@users.noreply.github.com>
2023-09-28 22:08:25 -0400

    rust update

--- order-routing/rust/fulfillment-constraints/default/src/main.rs
+++ order-routing/rust/fulfillment-constraints/default/src/main.rs
@@ -1,24 +1,7 @@
-use shopify_function::prelude::*;
-use shopify_function::Result;
+use std::process;
+pub mod run;
 
-use serde::{Deserialize, Serialize};
-
-generate_types!(
-    query_path = "./input.graphql",
-    schema_path = "./schema.graphql"
-);
-
-#[derive(Serialize, Deserialize, Default, PartialEq)]
-struct Config {}
-
-#[shopify_function]
-fn function(_input: input::ResponseData) -> Result<output::FunctionResult> {
-    let operations = vec![];
-
-    // Build operations based on the input query response here.
-
-    Ok(output::FunctionResult { operations })
+fn main() {
+    eprintln!("Please invoke a named export.");
+    process::exit(1);
 }
-
-#[cfg(test)]
-mod tests;
