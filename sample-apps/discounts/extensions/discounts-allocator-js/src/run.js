c219e1a48111527711a7994c26e86790fab2cdaa
Mathius Johnson <mathius.johnson@shopify.com>
2024-01-24 17:59:22 +0000

    feat: add javascript discounts allocator function extension

---
+++ sample-apps/discounts/extensions/discounts-allocator-js/src/run.js
@@ -0,0 +1,107 @@
+import {Decimal} from 'decimal.js';
+
+const TOTAL_DISCOUNTS_CAP_REACHED =
+  'Maximum discount limit reached for this cart';
+const SINGLE_DISCOUNT_CAP_REACHED =
+  'Maximum discount limit reached for this discount';
+
+function getTargetLineIndex(target) {
+  return parseInt(target.cartLineId.slice(-1));
+}
+
+function calculateCurrentTargetPrice(inputCartLines, target) {
+  const targetLineIndex = getTargetLineIndex(target);
+  const targetLine = inputCartLines[targetLineIndex];
+  return targetLine.cost.amountPerQuantity.amount * target.quantity;
+}
+
+export function run(input) {
+  let totalDiscountsCap = parseFloat(input.shop.metafield?.value ?? '-1');
+  let totalDiscount = 0.0;
+
+  let allLinesOutputDiscounts = input.cart.lines.map(line => ({
+    cartLineId: line.id,
+    quantity: line.quantity,
+    allocations: [],
+  }));
+  let displayableErrors = [];
+
+  for (const discount of input.discounts) {
+    let currentDiscountCap = parseFloat(discount.metafield?.value ?? '-1');
+    let currentDiscountTotal = 0.0;
+
+    for (const proposal of discount.discountProposals) {
+      const totalTargetsPrice = proposal.targets.reduce((total, target) => {
+        return total + calculateCurrentTargetPrice(input.cart.lines, target);
+      }, 0);
+
+      for (const target of proposal.targets) {
+        const currentTargetPrice = calculateCurrentTargetPrice(
+          input.cart.lines,
+          target,
+        );
+        const currentTargetRatio = currentTargetPrice / totalTargetsPrice;
+
+        let lineDiscountAmount = 0.0;
+        if (proposal.valueType === 'FIXED_AMOUNT') {
+          lineDiscountAmount = proposal.value * currentTargetRatio;
+        } else if (proposal.valueType === 'PERCENTAGE') {
+          lineDiscountAmount =
+            (proposal.value / 100.0) * totalTargetsPrice * currentTargetRatio;
+        }
+
+        if (
+          currentDiscountCap >= 0.0 &&
+          currentDiscountTotal + lineDiscountAmount > currentDiscountCap
+        ) {
+          lineDiscountAmount = currentDiscountCap - currentDiscountTotal;
+          displayableErrors.push({
+            discountId: discount.id.toString(),
+            reason: SINGLE_DISCOUNT_CAP_REACHED,
+          });
+        }
+
+        if (
+          totalDiscountsCap >= 0.0 &&
+          totalDiscount + lineDiscountAmount > totalDiscountsCap
+        ) {
+          lineDiscountAmount = totalDiscountsCap - totalDiscount;
+          displayableErrors.push({
+            discountId: discount.id.toString(),
+            reason: TOTAL_DISCOUNTS_CAP_REACHED,
+          });
+        }
+
+        if (lineDiscountAmount === 0.0) {
+          continue;
+        }
+
+        totalDiscount += lineDiscountAmount;
+        currentDiscountTotal += lineDiscountAmount;
+
+        const targetLineIndex = getTargetLineIndex(target);
+        const targetSequence =
+          allLinesOutputDiscounts[targetLineIndex].allocations.length;
+        const targetAllocation = {
+          discountProposalId: proposal.handle,
+          amount: new Decimal(lineDiscountAmount),
+          sequence: targetSequence,
+        };
+        allLinesOutputDiscounts[targetLineIndex].allocations.push(
+          targetAllocation,
+        );
+      }
+    }
+  }
+
+  const lineDiscounts = allLinesOutputDiscounts.filter(
+    outputDiscount => outputDiscount.allocations.length > 0,
+  );
+
+  const output = {
+    lineDiscounts,
+    displayableErrors,
+  };
+
+  return output;
+}
---
+++ sample-apps/discounts/extensions/discounts-allocator-js/src/run.test.js
@@ -0,0 +1,265 @@
+import {describe, it, expect} from 'vitest';
+import {run} from './run';
+import Decimal from 'decimal.js';
+
+describe('discount function', () => {
+  it('test result without cart max', () => {
+    const result = run({
+      discounts: [
+        {
+          id: 'gid://shopify/DiscountNode/13',
+          title: 'CAPS',
+          code: 'CAPS',
+          type: 'CodePriceRule',
+          discountApplicationStrategy: 'FIRST',
+          discountProposals: [
+            {
+              handle: '8d783e44-14b9-4c2c-b431-2be340f1f4e1',
+              message: 'CAPS',
+              valueType: 'FIXED_AMOUNT',
+              value: '10.0',
+              targets: [
+                {
+                  cartLineId: 'gid://shopify/CartLine/0',
+                  quantity: 5,
+                },
+              ],
+            },
+          ],
+        },
+        {
+          id: 'gid://shopify/DiscountNode/8',
+          title: 'TIES',
+          code: 'TIES',
+          type: 'CodePriceRule',
+          discountApplicationStrategy: 'FIRST',
+          discountProposals: [
+            {
+              handle: 'a7194cec-d807-4c46-9284-4fec7a3f5fbe',
+              message: 'TIES',
+              valueType: 'PERCENTAGE',
+              value: '20.0',
+              targets: [
+                {
+                  cartLineId: 'gid://shopify/CartLine/1',
+                  quantity: 6,
+                },
+              ],
+            },
+          ],
+        },
+      ],
+      presentmentCurrencyRate: '1.2706752',
+      cart: {
+        lines: [
+          {
+            id: 'gid://shopify/CartLine/0',
+            quantity: 5,
+            cost: {
+              amountPerQuantity: {
+                amount: '32.0',
+                currencyCode: 'CAD',
+              },
+            },
+            merchandise: {
+              __typename: 'ProductVariant',
+              id: 'gid://shopify/ProductVariant/17',
+              title: 'Shorts',
+              product: {
+                id: 'gid://shopify/Product/9',
+                title: 'Shorts',
+              },
+            },
+          },
+          {
+            id: 'gid://shopify/CartLine/1',
+            quantity: 6,
+            cost: {
+              amountPerQuantity: {
+                amount: '20.0',
+                currencyCode: 'CAD',
+              },
+            },
+            merchandise: {
+              __typename: 'ProductVariant',
+              id: 'gid://shopify/ProductVariant/17',
+              title: 'Shorts',
+              product: {
+                id: 'gid://shopify/Product/9',
+                title: 'Shorts',
+              },
+            },
+          },
+        ],
+      },
+      shop: {
+        metafield: null,
+      },
+    });
+
+    const expected = {
+      lineDiscounts: [
+        {
+          cartLineId: 'gid://shopify/CartLine/0',
+          quantity: 5,
+          allocations: [
+            {
+              discountProposalId: '8d783e44-14b9-4c2c-b431-2be340f1f4e1',
+              amount: new Decimal(10.0),
+              sequence: 0,
+            },
+          ],
+        },
+        {
+          cartLineId: 'gid://shopify/CartLine/1',
+          quantity: 6,
+          allocations: [
+            {
+              discountProposalId: 'a7194cec-d807-4c46-9284-4fec7a3f5fbe',
+              amount: new Decimal(24.0),
+              sequence: 0,
+            },
+          ],
+        },
+      ],
+      displayableErrors: [],
+    };
+
+    expect(result).toEqual(expected);
+  });
+
+  it('test result with 10 cart max', () => {
+    const result = run({
+      discounts: [
+        {
+          id: 'gid://shopify/DiscountNode/13',
+          title: 'CAPS',
+          code: 'CAPS',
+          type: 'CodePriceRule',
+          discountApplicationStrategy: 'FIRST',
+          discountProposals: [
+            {
+              handle: '8d783e44-14b9-4c2c-b431-2be340f1f4e1',
+              message: 'CAPS',
+              valueType: 'PERCENTAGE',
+              value: '5.0',
+              targets: [
+                {
+                  cartLineId: 'gid://shopify/CartLine/0',
+                  quantity: 5,
+                },
+              ],
+            },
+          ],
+        },
+        {
+          id: 'gid://shopify/DiscountNode/8',
+          title: 'TIES',
+          code: 'TIES',
+          type: 'CodePriceRule',
+          discountApplicationStrategy: 'FIRST',
+          discountProposals: [
+            {
+              handle: 'a7194cec-d807-4c46-9284-4fec7a3f5fbe',
+              message: 'TIES',
+              valueType: 'PERCENTAGE',
+              value: '10.0',
+              targets: [
+                {
+                  cartLineId: 'gid://shopify/CartLine/1',
+                  quantity: 6,
+                },
+              ],
+            },
+          ],
+        },
+      ],
+      presentmentCurrencyRate: '1.2706752',
+      cart: {
+        lines: [
+          {
+            id: 'gid://shopify/CartLine/0',
+            quantity: 5,
+            cost: {
+              amountPerQuantity: {
+                amount: '32.0',
+                currencyCode: 'CAD',
+              },
+            },
+            merchandise: {
+              __typename: 'ProductVariant',
+              id: 'gid://shopify/ProductVariant/17',
+              title: 'Shorts',
+              product: {
+                id: 'gid://shopify/Product/9',
+                title: 'Shorts',
+              },
+            },
+          },
+          {
+            id: 'gid://shopify/CartLine/1',
+            quantity: 6,
+            cost: {
+              amountPerQuantity: {
+                amount: '20.0',
+                currencyCode: 'CAD',
+              },
+            },
+            merchandise: {
+              __typename: 'ProductVariant',
+              id: 'gid://shopify/ProductVariant/17',
+              title: 'Shorts',
+              product: {
+                id: 'gid://shopify/Product/9',
+                title: 'Shorts',
+              },
+            },
+          },
+        ],
+      },
+      shop: {
+        localTime: {
+          date: '2023-11-15',
+        },
+        metafield: {
+          value: '10.0',
+        },
+      },
+    });
+
+    const expected = {
+      lineDiscounts: [
+        {
+          cartLineId: 'gid://shopify/CartLine/0',
+          quantity: 5,
+          allocations: [
+            {
+              discountProposalId: '8d783e44-14b9-4c2c-b431-2be340f1f4e1',
+              amount: new Decimal(8.0),
+              sequence: 0,
+            },
+          ],
+        },
+        {
+          cartLineId: 'gid://shopify/CartLine/1',
+          quantity: 6,
+          allocations: [
+            {
+              discountProposalId: 'a7194cec-d807-4c46-9284-4fec7a3f5fbe',
+              amount: new Decimal(2.0),
+              sequence: 0,
+            },
+          ],
+        },
+      ],
+      displayableErrors: [
+        {
+          discountId: 'gid://shopify/DiscountNode/8',
+          reason: 'Maximum discount limit reached for this cart',
+        },
+      ],
+    };
+
+    expect(result).toEqual(expected);
+  });
+});
