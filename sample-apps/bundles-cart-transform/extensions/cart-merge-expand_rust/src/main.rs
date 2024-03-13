849dbc82064942e123973119f60354068e5c0cf9
Patrick Lin <patrick.lin@shopify.com>
2024-03-13 20:22:00 +0000

    Allow Merge Bundles to share Cart Lines that have the same Product Variant

--- sample-apps/bundles-cart-transform/extensions/cart-merge-expand_rust/src/main.rs
+++ sample-apps/bundles-cart-transform/extensions/cart-merge-expand_rust/src/main.rs
@@ -1,4 +1,5 @@
 use crate::input::InputCart as Cart;
+use crate::input::InputCartLines as CartLine;
 use crate::input::InputCartLinesMerchandise::ProductVariant;
 use crate::input::InputCartLinesMerchandiseOnProductVariant;
 use crate::output::CartLineInput;
@@ -11,6 +12,7 @@ use crate::output::PriceAdjustmentValue;
 use serde::Deserialize;
 use shopify_function::prelude::*;
 use shopify_function::Result;
+use std::collections::HashMap;
 
 generate_types!(
     query_path = "./input.graphql",
@@ -66,10 +68,16 @@ fn function(input: input::ResponseData) -> Result<output::FunctionResult> {
 
 fn get_merge_cart_operations(cart: &Cart) -> impl Iterator<Item = CartOperation> + '_ {
     let merge_parent_defintions = get_merge_parent_definitions(cart);
+
+    let mut cart_lines: Vec<CartLine> = Vec::new();
+    cart.lines
+        .iter()
+        .for_each(|line| cart_lines.push(line.clone()));
+
     merge_parent_defintions
         .into_iter()
-        .filter_map(|definition| {
-            let components_in_cart = get_components_in_cart(cart, &definition);
+        .filter_map(move |definition| {
+            let components_in_cart = get_components_in_cart(&mut cart_lines, &definition);
             (components_in_cart.len() == definition.component_reference.len()).then(|| {
                 let cart_lines: Vec<CartLineInput> = components_in_cart
                     .into_iter()
@@ -100,20 +108,55 @@ fn get_merge_cart_operations(cart: &Cart) -> impl Iterator<Item = CartOperation>
         })
 }
 
-fn get_components_in_cart(cart: &Cart, definition: &ComponentParent) -> Vec<CartLineInput> {
-    definition
+fn get_components_in_cart(
+    cart_lines: &mut Vec<CartLine>,
+    definition: &ComponentParent,
+) -> Vec<CartLineInput> {
+    let line_results: Vec<CartLineInput> = definition
         .component_reference
         .iter()
         .zip(definition.component_quantities.iter())
         .filter_map(|(reference, &quantity)| {
-            cart.lines.iter().find_map(move |line| {
+          cart_lines.iter().find_map(move |line| {
                 matches!(
                     &line.merchandise,
                     ProductVariant(merchandise) if reference == &merchandise.id && line.quantity >= quantity,
                 ).then(|| CartLineInput { cart_line_id: line.id.clone(), quantity })
             })
         })
-        .collect()
+        .collect();
+
+    update_cart_lines_from_function_result(cart_lines, line_results.clone());
+
+    return line_results;
+}
+
+fn update_cart_lines_from_function_result(
+    cart_lines: &mut Vec<CartLine>,
+    line_results: Vec<CartLineInput>,
+) {
+    let mut cart_line_tracker: HashMap<String, i64> = cart_lines
+        .iter()
+        .map(|cart_line| (cart_line.id.clone(), cart_line.quantity))
+        .collect();
+
+    for line_result in line_results.iter() {
+        if let Some(target_cart_line) = cart_lines
+            .iter()
+            .find(|cart_line| cart_line.id == line_result.cart_line_id)
+        {
+            let new_quantity = cart_line_tracker[&target_cart_line.id] - line_result.quantity;
+            cart_line_tracker.insert(target_cart_line.id.clone(), new_quantity);
+        }
+    }
+
+    cart_lines.retain(|line| cart_line_tracker[&line.id] > 0);
+
+    for cart_line in cart_lines.iter_mut() {
+        if cart_line_tracker[&cart_line.id] > 0 {
+            cart_line.quantity = cart_line_tracker[&cart_line.id];
+        }
+    }
 }
 
 fn get_merge_parent_definitions(cart: &Cart) -> Vec<ComponentParent> {
@@ -125,7 +168,6 @@ fn get_merge_parent_definitions(cart: &Cart) -> Vec<ComponentParent> {
         }
     }
 
-    merge_parent_defintions.dedup_by(|a, b| a.id == b.id);
     merge_parent_defintions
 }
 
