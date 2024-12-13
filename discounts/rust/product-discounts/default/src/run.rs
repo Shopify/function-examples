555a019a795c607f68d3b3b9a54e8b132826a9f1
Adam Petro <adam.petro@shopify.com>
2024-12-13 15:23:48 -0500

    bluejay demo

--- discounts/rust/product-discounts/default/src/run.rs
+++ discounts/rust/product-discounts/default/src/run.rs
@@ -3,31 +3,38 @@ use shopify_function::Result;
 
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize, Default, PartialEq)]
+#[derive(Serialize, Deserialize, Default, PartialEq, Clone, Debug)]
 #[serde(rename_all(deserialize = "camelCase"))]
 struct Configuration {}
 
-impl Configuration {
-    fn from_str(value: &str) -> Self {
-        serde_json::from_str(value).expect("Unable to parse configuration value from metafield")
-    }
+#[typegen("schema.graphql")]
+mod schema {
+    #[query(
+        "src/run.graphql",
+        custom_scalar_overrides = {
+            "Input.discountNode.metafield.jsonValue" => super::Configuration,
+        }
+    )]
+    pub mod run {}
 }
 
-#[shopify_function_target(query_path = "src/run.graphql", schema_path = "schema.graphql")]
-fn run(input: input::ResponseData) -> Result<output::FunctionRunResult> {
-    let no_discount = output::FunctionRunResult {
+#[shopify_function]
+fn run(input: schema::run::Input) -> Result<schema::FunctionRunResult> {
+    let no_discount = schema::FunctionRunResult {
         discounts: vec![],
-        discount_application_strategy: output::DiscountApplicationStrategy::FIRST,
+        discount_application_strategy: schema::DiscountApplicationStrategy::First,
     };
 
-    let _config = match input.discount_node.metafield {
-        Some(input::InputDiscountNodeMetafield { value }) => Configuration::from_str(&value),
-        None => return Ok(no_discount),
+    let Some(schema::run::input::discount_node::Metafield {
+        json_value: _config,
+    }) = input.discount_node.metafield
+    else {
+        return Ok(no_discount);
     };
 
-    Ok(output::FunctionRunResult {
+    Ok(schema::FunctionRunResult {
         discounts: vec![],
-        discount_application_strategy: output::DiscountApplicationStrategy::FIRST,
+        discount_application_strategy: schema::DiscountApplicationStrategy::First,
     })
 }
 
@@ -38,8 +45,6 @@ mod tests {
 
     #[test]
     fn test_result_contains_no_discounts() -> Result<()> {
-        use run::output::*;
-
         let result = run_function_with_input(
             run,
             r#"
@@ -50,9 +55,9 @@ mod tests {
                 }
             "#,
         )?;
-        let expected = FunctionRunResult {
+        let expected = schema::FunctionRunResult {
             discounts: vec![],
-            discount_application_strategy: DiscountApplicationStrategy::FIRST,
+            discount_application_strategy: schema::DiscountApplicationStrategy::First,
         };
 
         assert_eq!(result, expected);
